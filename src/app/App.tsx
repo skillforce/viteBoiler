99713c834724504405b17a9599c1ce3a8f397d88
skillforce <denis_tatarinov@stylesoft.by>
2024-10-23 12:27:26 +0300

    hello world console log

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import { appBrowserRouterConfig } from '@app/router/router.tsx';
 import { RouterProvider } from 'react-router-dom';
 
 function App() {
+    console.log('hello world');
     return <RouterProvider router={appBrowserRouterConfig} />;
 }
 
