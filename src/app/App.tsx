01e1d1e797fc9564540ff974d457ff1c8be05ba1
skillforce <denis_tatarinov@stylesoft.by>
2024-10-23 12:31:33 +0300

    lint-stage set_up

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,8 +2,6 @@ import { appBrowserRouterConfig } from '@app/router/router.tsx';
 import { RouterProvider } from 'react-router-dom';
 
 function App() {
-    console.log('hello world');
-    const sdcsd = 'dscdscsdc';
     return <RouterProvider router={appBrowserRouterConfig} />;
 }
 
