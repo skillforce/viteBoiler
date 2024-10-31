48b4e66de7581ae5cd59e8d7c48534bfc76ebbae
skillforce <denis_tatarinov@stylesoft.by>
2024-10-31 10:59:29 +0300

    +vite config for build modified

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,31 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
-const root = path.resolve(__dirname, 'src');
+const rootSrc = path.resolve(__dirname, 'src');
+const entryIndexFilePath = resolve(__dirname, 'index.html');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    resolve: {
-        alias: {
-            '@': root,
-            '@app': path.join(root, 'app'),
-            '@pages': path.join(root, 'pages'),
-            '@entities': path.join(root, 'entities'),
-            '@features': path.join(root, 'features'),
-            '@shared': path.join(root, 'shared'),
-        },
-    },
     base: './',
     plugins: [react()],
+    build: {
+        rollupOptions: {
+            input: {
+                main: entryIndexFilePath,
+            },
+        },
+    },
+    resolve: {
+        alias: {
+            '@': rootSrc,
+            '@app': path.join(rootSrc, 'app'),
+            '@pages': path.join(rootSrc, 'pages'),
+            '@entities': path.join(rootSrc, 'entities'),
+            '@features': path.join(rootSrc, 'features'),
+            '@shared': path.join(rootSrc, 'shared'),
+        },
+    },
     css: {
         preprocessorOptions: {
             scss: {
